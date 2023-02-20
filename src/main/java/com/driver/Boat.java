221015265bdbe3e37a04480791b09e48adaea3a3
somuip <117620797+somuip@users.noreply.github.com>
2023-02-20 09:44:16 +0530

    solution 1

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -6,6 +6,11 @@ public class Boat implements WaterVehicle{
     private String name;
     private int capacity;
 
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
     public String getName() {
         return name;
     }
